96ce56ce905bc1dc3a3722855767254443a78bcd
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-04-04 15:18:31 -0700

    More work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Example from "@/components/practice"
+import Example from "@/components/practice";
 
 const Home = () => {
   return (
-    <div className=" h-screen w-screen items-center justify-center">
+    <div className="h-screen w-screen items-center justify-center">
       Practice
       <Example text1="Prop1" text2="Prop2" text3="Props"></Example>
     </div>
-    
   );
 };
 
--- src/components/practice.tsx
+++ src/components/practice.tsx
@@ -1,15 +1,13 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-red-100 flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-blue-200 w-1/2 p-5 text-left">{props.text1}</div>
+    <div className="flex w-3/4 items-center gap-1 justify-self-center bg-red-100 p-8">
+      <div className="w-1/2 bg-blue-200 p-5 text-left">{props.text1}</div>
 
       <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-green-200 w-full p-5 text-left">
-          {props.text2}
-        </div>
+        <div className="w-full bg-green-200 p-5 text-left">{props.text2}</div>
       </div>
 
-      <div className="bg-yellow-200 w-1/2 p-5">{props.text3}</div>
+      <div className="w-1/2 bg-yellow-200 p-5">{props.text3}</div>
     </div>
   );
 };
