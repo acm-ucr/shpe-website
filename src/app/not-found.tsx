2ae4caa8ed1ca2ad96247859b7dc47b5d76573f1
joshuaschumperlid8 <joshua.schumperlid8@gmail.com>
2026-04-15 16:07:09 -0700

    404 Page Finished

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,12 +11,14 @@ const NotFound = () => {
         alt = "404 Image"
         className="z-10 h-auto w-1/2 pb-10"
       />}
-      <p className="w-1/2 font-shpe-beachwood font-semibold text-blue-800 text-5xl text-center">
+      <p className="mt-5 mb-5 w-1/2 font-shpe-beachwood font-semibold text-blue-800 text-4xl text-center">
         UH OH IT LOOKS LIKE THE PAGE YOU ARE LOOKING FOR DOESN'T EXIST
       </p>
       <Link
         href="/"
-        className="mt-5 flex justify-center w-25 h-10 rounded-2xl bg-amber-600 text-white"
+        className="mt-5 flex justify-center items-center w-30 h-12
+        rounded-3xl bg-gradient-to-r from-orange-500 via-orange-600 to-orange-700 
+        text-white font-shpe-univers font-light text-xl"
       >
         HOME
       </Link>
