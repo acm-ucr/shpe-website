d3190b2a4070f8d36d5092c596f26b4ced104726
joshuaschumperlid8 <joshua.schumperlid8@gmail.com>
2026-04-15 16:08:37 -0700

    404 Page Formated and Finished

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,28 +3,26 @@ import NotFoundImage from "@/public/404Image.webp";
 import Link from "next/link";
 
 const NotFound = () => {
-  return(
-    
-    <div className="flex items-center justify-center h-screen flex-col">
-      { <Image
-        src = {NotFoundImage}
-        alt = "404 Image"
-        className="z-10 h-auto w-1/2 pb-10"
-      />}
-      <p className="mt-5 mb-5 w-1/2 font-shpe-beachwood font-semibold text-blue-800 text-4xl text-center">
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      {
+        <Image
+          src={NotFoundImage}
+          alt="404 Image"
+          className="z-10 h-auto w-1/2 pb-10"
+        />
+      }
+      <p className="font-shpe-beachwood mt-5 mb-5 w-1/2 text-center text-4xl font-semibold text-blue-800">
         UH OH IT LOOKS LIKE THE PAGE YOU ARE LOOKING FOR DOESN'T EXIST
       </p>
       <Link
         href="/"
-        className="mt-5 flex justify-center items-center w-30 h-12
-        rounded-3xl bg-gradient-to-r from-orange-500 via-orange-600 to-orange-700 
-        text-white font-shpe-univers font-light text-xl"
+        className="font-shpe-univers mt-5 flex h-12 w-30 items-center justify-center rounded-3xl bg-gradient-to-r from-orange-500 via-orange-600 to-orange-700 text-xl font-light text-white"
       >
         HOME
       </Link>
     </div>
   );
-  
 };
 
 export default NotFound;
