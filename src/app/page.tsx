7c8db0e0ec2de1ac2624702673ab96ce953bb798
etan29 <emilytan2992@gmail.com>
2026-04-17 04:45:20 -0700

    create board card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import BoardCard from "@/components/board/BoardCard";
+import { boardData } from "@/data/boardData";
 import Example from "@/components/practice";
 
 const Home = () => {
@@ -5,6 +7,21 @@ const Home = () => {
     <div className="h-screen w-screen items-center justify-center">
       Practice
       <Example text1="Prop1" text2="Prop2" text3="Props"></Example>
+
+      <div className="flex w-full justify-center px-6">
+        <div className="grid grid-cols-3 gap-x-40 gap-y-10">
+          {boardData.map((member, index) => (
+            <BoardCard
+              key={index}
+              name={member.name}
+              image={member.image}
+              position={member.position}
+              linkedin={member.linkedin}
+            />
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 };
---
+++ src/components/board/BoardCard.tsx
@@ -0,0 +1,37 @@
+import Image, { StaticImageData } from "next/image";
+import LinkedInIcon from "@/public/linkedIn.webp"; 
+
+interface BoardCardProps {
+  name: string;
+  image: StaticImageData | string;
+  position: string;
+  linkedin: string;
+}
+
+const BoardCard = ({ name, image, position, linkedin }: BoardCardProps) => {
+  return (
+    <div className="flex w-80 flex-col items-center rounded-4xl bg-[#001F5B] p-7 pb-3 text-center text-white">
+      <div className="relative mb-3 aspect-[5/6] w-full overflow-hidden rounded-3xl bg-[#D9D9D9]">
+        <Image
+          src={image}
+          alt={name}
+          fill
+          className="object-cover"
+        />
+      </div>
+      
+      <div className="text-2xl font-bold">{name}</div>
+      <div className="text-lg">{position}</div>
+
+      <a href={linkedin} target="_blank" rel="noreferrer">
+        <Image 
+          src={LinkedInIcon} 
+          alt="LinkedIn icon" 
+          className="h-12 w-12" 
+        />
+      </a> 
+    </div>
+  );
+};
+
+export default BoardCard;
