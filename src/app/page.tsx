7d637195497e1d5c9025b4862458fd1dbb044cd7
faisalthe1 <127719559+faisalthe1@users.noreply.github.com>
2026-04-16 18:52:20 -0700

    fixed format and added ExampleComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import Example from "@/components/practice";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-6 bg-shpe-blue-050">
+    <div className="bg-shpe-blue-050 flex h-screen w-screen flex-col items-center justify-center gap-6">
       {/*Page has a light blue background*/}
-      
-      <Example text1="Prop 1" text2="Prop 2" text3="Prop 2" text4="Props"></Example>
+
+      <Example
+        text1="Prop 1"
+        text2="Prop 2"
+        text3="Prop 2"
+        text4="Props"
+      ></Example>
       {/*The component now gets all four props*/}
     </div>
   );
