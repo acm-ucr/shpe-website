bb332da7ff1696e9fa4b994e7cd0cc42cf69916a
Adam Schumperli <aschu042@ucr.edu>
2026-04-17 15:36:54 -0700

    variables for the icon sizes for easier changes

--- src/components/board/footer.tsx
+++ src/components/board/footer.tsx
@@ -8,6 +8,8 @@ import shpeFooterLogo from "@/public/shpeFooterLogo.webp";
 
 import Image from "next/image";
 
+const iconSize = "h-[50px] w-[50px]";
+
 const Footer = () => {
   return (
     <div className="from-shpe-blue-200 to-shpe-blue-050 fixed bottom-0 left-0 flex w-full items-center justify-between bg-gradient-to-r px-[40px] py-[20px]">
@@ -18,24 +20,12 @@ const Footer = () => {
       />
 
       <div className="flex items-center gap-[15px]">
-        <Image
-          src={instagramIcon}
-          alt="Instagram"
-          className="h-[50px] w-[50px]"
-        />
-        <Image src={newsIcon} alt="News" className="h-[50px] w-[50px]" />
-        <Image
-          src={linkedinIcon}
-          alt="LinkedIn"
-          className="h-[50px] w-[50px]"
-        />
-        <Image src={emailIcon} alt="Email" className="h-[50px] w-[50px]" />
-        <Image src={discordIcon} alt="Discord" className="h-[50px] w-[50px]" />
-        <Image
-          src={facebookIcon}
-          alt="Facebook"
-          className="h-[50px] w-[50px]"
-        />
+        <Image src={instagramIcon} alt="Instagram" className={iconSize} />
+        <Image src={newsIcon} alt="News" className={iconSize} />
+        <Image src={linkedinIcon} alt="LinkedIn" className={iconSize} />
+        <Image src={emailIcon} alt="Email" className={iconSize} />
+        <Image src={discordIcon} alt="Discord" className={iconSize} />
+        <Image src={facebookIcon} alt="Facebook" className={iconSize} />
       </div>
     </div>
   );
