cbbade22d73b960e6c8afc553760b842758fe136
Lkarm003 <lkarm003@ucr.edu>
2026-04-16 20:33:36 -0700

    followed all the instructions to make changes

--- src/components/practice.tsx
+++ src/components/practice.tsx
@@ -1,15 +1,24 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 justify-self-center bg-red-100 p-8">
-      <div className="w-1/2 bg-blue-200 p-5 text-left">{props.text1}</div>
-
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">{props.text2}</div>
+    <div className="bg-shpe-cyan-300 flex w-2/3 flex-col items-center gap-1 gap-4 justify-self-center rounded-md p-8">
+      <div className="bg-shpe-orange-500 w-1/2 p-5 text-center text-lg">
+        {props.text1}
       </div>
 
-      <div className="w-1/2 bg-yellow-200 p-5">{props.text3}</div>
+      {/* 1. added another div for Prop2 and made it into rows*/}
+      <div className="flex w-1/2 flex-row gap-1">
+        <div className="bg-shpe-red-600 w-full rounded-full p-5 text-center text-lg">
+          {props.text2}
+        </div>
+        <div className="bg-shpe-blue-100 w-full rounded-full p-5 text-center text-lg">
+          {props.text2}
+        </div>
+      </div>
+
+      <div className="bg-shpe-blue-900 w-1/2 p-5 text-center text-lg text-white">
+        {props.text3}
+      </div>
     </div>
   );
 };
-
 export default Example;
