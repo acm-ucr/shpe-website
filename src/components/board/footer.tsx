f09f7086d7768c89f29964384821e00c2c7386f5
Adam Schumperli <aschu042@ucr.edu>
2026-04-17 18:22:44 -0700

    added mobile responsiveness to the footer

--- src/components/board/footer.tsx
+++ src/components/board/footer.tsx
@@ -12,14 +12,14 @@ const iconSize = "h-[50px] w-[50px]";
 
 const Footer = () => {
   return (
-    <div className="from-shpe-blue-200 to-shpe-blue-150 fixed bottom-0 left-0 flex w-full items-center justify-between bg-gradient-to-r px-[40px] py-[20px]">
+    <div className="from-shpe-blue-200 to-shpe-blue-150 fixed bottom-0 left-0 flex w-full flex-col items-center justify-center bg-gradient-to-r px-[20px] py-[15px] md:flex-row md:justify-between md:px-[40px] md:py-[20px]">
       <Image
         src={shpeFooterLogo}
         alt="FooterLogo"
         className="h-[60px] w-[195px]"
       />
 
-      <div className="flex items-center gap-[15px]">
+      <div className="flex w-full justify-around md:w-auto md:justify-end md:gap-[15px]">
         <Image src={instagramIcon} alt="Instagram" className={iconSize} />
         <Image src={newsIcon} alt="News" className={iconSize} />
         <Image src={linkedinIcon} alt="LinkedIn" className={iconSize} />
