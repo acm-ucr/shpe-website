e27a1d0cf6edd5289890dea8191006fd3fac020a
Adam Schumperli <aschu042@ucr.edu>
2026-04-20 10:40:23 -0700

    added in text for the component and ran npm run format to properly format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/board/footer";
+import ContactUs from "@/components/about/contactUs";
 
 const Home = () => {
   return (
     <div className="h-screen w-screen items-center justify-center">
       Practice
       <Footer></Footer>
+      <ContactUs></ContactUs>
     </div>
   );
 };
--- src/components/about/contactUs.tsx
+++ src/components/about/contactUs.tsx
@@ -10,11 +10,19 @@ import newsIcon from "@/public/footer/news.webp";*/
 //const iconSize = "h-[50px] w-[50px]";
 
 const ContactUs = () => {
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <div className="bg-white">
+      <div className="font-shpe-beachwood text-center text-6xl font-semibold">
+        CONTACT US
+      </div>
+      <div className="font-shpe-univers text-shpe-blue-200 text-center text-3xl">
+        Have Questions?
+      </div>
+      <div className="font-shpe-univers text-shpe-blue-200 text-center text-3xl">
+        Connect With Us!
+      </div>
+    </div>
+  );
 };
 
-export default ContactUs
+export default ContactUs;
