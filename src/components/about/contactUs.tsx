d5e8be73acd0951e91b6437c65146ac0f9a6e662
Adam Schumperli <aschu042@ucr.edu>
2026-04-20 10:25:23 -0700

    made contactUs file for the component and added in icons needed for the component

---
+++ src/components/about/contactUs.tsx
@@ -0,0 +1,20 @@
+/*import discordIcon from "@/public/footer/discord.webp";
+import emailIcon from "@/public/footer/email.webp";
+import facebookIcon from "@/public/footer/facebook.webp";
+import instagramIcon from "@/public/footer/instagram.webp";
+import linkedinIcon from "@/public/footer/linkedIn.webp";
+import newsIcon from "@/public/footer/news.webp";*/
+
+//import Image from "next/image";
+
+//const iconSize = "h-[50px] w-[50px]";
+
+const ContactUs = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ContactUs
