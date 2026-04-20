d47b4e417c8f0397034cfff9358da324274421ec
Adam Schumperli <aschu042@ucr.edu>
2026-04-20 12:47:45 -0700

    redid the work by using mapping for images and lines seperating, also reformated code so it is more readable and easier to change

--- src/components/about/contactUs.tsx
+++ src/components/about/contactUs.tsx
@@ -7,66 +7,106 @@ import newsIcon from "@/public/footer/news.webp";
 import gearImage from "@/public/gear.webp";
 
 import Image from "next/image";
+import { StaticImageData } from "next/image";
 
 // made these into variables for easier changes if needed/wanted
 const iconSize = "h-[88px] w-[88px]";
-const circleStyle = "flex flex-col items-center justify-center bg-shpe-orange-500 rounded-full h-[140px] w-[140px]";
+const circleStyle =
+  "flex flex-col items-center justify-center bg-shpe-orange-500 rounded-full h-[140px] w-[140px]";
 const lineStyle = "h-[3px] w-[82px] bg-shpe-orange-500 mx-[20px]";
-const subHeaderStyle = "font-shpe-univers text-shpe-blue-200 text-center text-3xl";
+const subHeaderStyle =
+  "font-shpe-univers text-shpe-blue-200 text-center text-3xl";
 const margins = "flex flex-col items-center gap-[20px] mt-[40px]";
+
+// reusable components
+type ContactCircleProps = {
+  icon: StaticImageData;
+  alt: string;
+  label: string;
+  textSize?: string;
+  tweak?: string;
+};
+const ContactCircle = ({
+  icon,
+  alt,
+  label,
+  textSize = "text-[17px]",
+  tweak = "-mt-[5px] pb-[5px]",
+}: ContactCircleProps) => (
+  <div className={circleStyle}>
+    <Image src={icon} alt={alt} className={`${iconSize}`} />
+    <div className={`text-center ${textSize} leading-none text-white ${tweak}`}>
+      {label}
+    </div>
+  </div>
+);
+
+const Divider = () => <div className={lineStyle} />;
+
+// data for reusable components
+const contactQuestions = [
+  {
+    icon: instagramIcon,
+    alt: "Instagram",
+    label: "@shpeucr",
+    textSize: "text-[15px]",
+    tweak: "-mt-[5px] pb-[10px]",
+  },
+  {
+    icon: emailIcon,
+    alt: "Email",
+    label: "shpeucr@gmail.com",
+    textSize: "text-[12px]",
+    tweak: "-mt-[15px] pb-[12px]",
+  },
+];
+
+const socialLinks = [
+  { icon: discordIcon, alt: "Discord", label: "Discord" },
+  { icon: newsIcon, alt: "Newsletter", label: "Newsletter" },
+  { icon: linkedinIcon, alt: "LinkedIn", label: "LinkedIn" },
+  { icon: facebookIcon, alt: "Facebook", label: "Facebook" },
+];
 
 const ContactUs = () => {
   return (
-    <div className="bg-shpe-white-100 relative overflow-hidden pb-[240px] pt-[40px]">
-        <Image src={gearImage} alt="gear" className="absolute left-[-100px] top-[50px] w-[300px] opacity-40 scale-x-[-1]"/>
-        <Image src={gearImage} alt="gear" className="absolute right-[-100px] top-[50px] w-[300px] opacity-40"/>
+    <div className="bg-shpe-white-100 relative overflow-hidden pt-[40px] pb-[240px]">
+      {/* Gears */}
+      <Image
+        src={gearImage}
+        alt="gear"
+        className="absolute top-[50px] left-[-100px] w-[300px] scale-x-[-1] opacity-40"
+      />
+      <Image
+        src={gearImage}
+        alt="gear"
+        className="absolute top-[50px] right-[-100px] w-[300px] opacity-40"
+      />
+      {/* Title */}
       <div className="font-shpe-beachwood text-center text-6xl font-semibold">
         CONTACT US
       </div>
-      <div className= {margins}>
-        <div className= {subHeaderStyle}>
-            Have Questions?
-        </div>
-        <div className="flex items-center justify-center mt-[25px]">
-            <div className={circleStyle}>
-                <Image src={instagramIcon} alt="Instagram" className={iconSize} />
-                <div className="text-center text-[15px] text-white leading-none -mt-[5px] pb-[10px]">
-                    @shpeucr
-                </div>
+      {/* Content */}
+      <div className={margins}>
+        <div className={subHeaderStyle}>Have Questions?</div>
+        <div className="mt-[25px] flex items-center justify-center">
+          {contactQuestions.map((item, i) => (
+            <div key={i} className="flex items-center">
+              <ContactCircle {...item} />
+              {i !== contactQuestions.length - 1 && <Divider />}
             </div>
-            <div className={lineStyle}></div>
-            <div className={circleStyle}>
-                <Image src={emailIcon} alt="Email" className= {iconSize} />
-                <div className="text-center text-[12px] text-white -mt-[15px] pb-[10px]">
-                    shpeucr@gmail.com
-                </div>
-            </div>
+          ))}
         </div>
       </div>
       <div className={margins}>
-        <div className={subHeaderStyle}>
-            Connect With Us!
-        </div>
-        <div className="flex items-center justify-center mt-[25px]">
-            <div className={circleStyle}>
-                <Image src={discordIcon} alt="Discord" className={iconSize} />
-                <div className="text-center text-[17px] text-white -mt-[7px] pb-[5px]">Discord</div>
+        <div className={subHeaderStyle}>Connect With Us!</div>
+        <div className="mt-[25px] flex items-center justify-center">
+          {socialLinks.map((item, i) => (
+            <div key={i} className="flex items-center">
+              <ContactCircle {...item} />
+              {i !== socialLinks.length - 1 && <Divider />}
             </div>
-            <div className={lineStyle}></div>
-            <div className={circleStyle}>
-                <Image src={newsIcon} alt="News" className={iconSize} />
-                <div className="text-center text-[17px] text-white -mt-[7px] pb-[5px]">Newsletter</div>
-            </div>
-            <div className={lineStyle}></div>
-            <div className={circleStyle}>
-                <Image src={linkedinIcon} alt="LinkedIn" className={iconSize} />
-                <div className="text-center text-[17px] text-white -mt-[7px] pb-[5px]">LinkedIn</div>
-            </div>
-            <div className={lineStyle}></div>
-            <div className={circleStyle}>
-                <Image src={facebookIcon} alt="Facebook" className={iconSize} />
-                <div className="text-center text-[17px] text-white -mt-[7px] pb-[5px]">Facebook</div>
-            </div>
+          ))}
         </div>
       </div>
     </div>
