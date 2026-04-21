9181a4bfb4f90a3d1d6fe5afcd603b35d1a69c03
Draylend <draylend0821@gmail.com>
2026-04-21 01:10:31 -0700

    Add popup component
    - Installed necessary libraries (react-icons, clsx, tailwind-merge, radix-ui)
    - Add dialog.tsx in src/components/ui/ for Dialog popup support
        - Edited out hard-coded positioning and styling
    - Add popup.tsx component kn src/components/ui/ for popups
        - Abstracts even more functionality for popups
        - Uses props to pass in two components (the trigger and result)
            - Can optionally add overlay background and positioning (default is center)
    - Add utils.ts in src/lib/ for cn library used in dialog.tsx

---
+++ src/components/ui/popup.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+  DialogOverlay,
+} from "@/components/ui/dialog";
+interface PopupProps {
+  clickableComponent: ReactNode;
+  popupComponent: ReactNode;
+  overlayColor?: string;
+  position?: string;
+}
+
+const Popup = ({
+  clickableComponent,
+  popupComponent,
+  overlayColor,
+  position,
+}: PopupProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger className="cursor-pointer">
+        {clickableComponent}
+      </DialogTrigger>
+      <DialogOverlay
+        className={`${overlayColor === undefined ? "bg-white/0" : overlayColor}`}
+      >
+        <DialogContent
+          className={`${position === undefined ? "fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" : position}`}
+        >
+          <DialogTitle />
+          {popupComponent}
+        </DialogContent>
+      </DialogOverlay>
+    </Dialog>
+  );
+};
+
+export default Popup;
