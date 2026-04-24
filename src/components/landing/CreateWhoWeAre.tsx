de11966f6575269755ceb3f2287ca87829d26a5a
Lkarm003 <lkarm003@ucr.edu>
2026-04-24 11:42:38 -0700

    changes

--- src/components/landing/CreateWhoWeAre.tsx
+++ src/components/landing/CreateWhoWeAre.tsx
@@ -1,17 +1,19 @@
-const CreateWhoWeAre = (props: { title: string; text: string}) => {
+const CreateWhoWeAre = (props: { title: string; text: string }) => {
   return (
-    <div className="flex items-start justify-between w-full py-20">
-        <div className="w-1/2 flex justify-start">
-            <img
-            src="/WhoWeAre.jpg"
-            alt="Who We Are"
-            className="shadow-lg max-w-full h-auto"
-            />
-        </div>
-        <div className="w-1/2 px-16">
-            <h1 className="font-shpe-beachwood text-shpe-blue-200 text-center mb-9 mt-15 scale-y-140 text-5xl font-semibold tracking-tight uppercase">{props.title}</h1>
-            <p className="text-xl leading-8 text-center px-10">{props.text}</p>
-        </div>
+    <div className="flex w-full items-start justify-between py-20">
+      <div className="flex w-1/2 justify-start">
+        <img
+          src="/WhoWeAre.jpg"
+          alt="Who We Are"
+          className="h-auto max-w-full shadow-lg"
+        />
+      </div>
+      <div className="w-1/2 px-16">
+        <h1 className="font-shpe-beachwood text-shpe-blue-200 mt-15 mb-9 scale-y-140 text-center text-5xl font-semibold tracking-tight uppercase">
+          {props.title}
+        </h1>
+        <p className="px-10 text-center text-xl leading-8">{props.text}</p>
+      </div>
     </div>
   );
 };
