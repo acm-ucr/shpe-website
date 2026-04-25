7ddbccd2dad8dd40274a081ee8a1ae97cc68b3c8
faisalthe1 <127719559+faisalthe1@users.noreply.github.com>
2026-04-25 15:25:30 -0700

    added header inside src/components/landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import Example from "@/components/practice";
+import LandingHeader from "@/components/landing/Header";
 
 const Home = () => {
   return (
-    <div className="bg-shpe-blue-050 flex h-screen w-screen flex-col items-center justify-center gap-6">
-      {/*Page has a light blue background*/}
-
-      <Example
-        text1="Prop 1"
-        text2="Prop 2"
-        text3="Prop 2"
-        text4="Props"
-      ></Example>
-      {/*The component now gets all four props*/}
-    </div>
+    <main className="bg-shpe-blue-100 min-h-screen">
+      <LandingHeader />
+    </main>
   );
 };
 
