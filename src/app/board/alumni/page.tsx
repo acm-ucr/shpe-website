604064d37969d92df13cf94b429f0a91c80ff2f8
owennusso <owennusso@gmail.com>
2026-05-02 10:00:48 -0700

    Created entry component, data file, and mapped it to alumni page

--- src/app/board/alumni/page.tsx
+++ src/app/board/alumni/page.tsx
@@ -1,9 +1,24 @@
-const Alumni = () => {
+import AlumniCard from "@/components/board/alumni";
+import { alumni } from "@/components/data/alumni";
+
+const AlumniPage = () => {
   return (
-    <div>
-      <p>Alumni</p>
+    <div className="flex flex-col items-center px-20 py-10">
+      <h2 className="font-shpe-beachwood text-shpe-blue-200 mb-8 scale-y-140 text-3xl font-semibold tracking-tight uppercase">
+        Our Alumni
+      </h2>
+      <div className="grid grid-cols-3 gap-6">
+        {alumni.map((alum) => (
+          <AlumniCard
+            key={alum.linkedIn}
+            name={alum.name}
+            year={alum.year}
+            linkedIn={alum.linkedIn}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default Alumni;
+export default AlumniPage;
