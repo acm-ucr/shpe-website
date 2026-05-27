12e4f3a50bd8109a3165e6cee88d8d857866fe6d
Adam Schumperli <aschu042@ucr.edu>
2026-05-26 20:29:51 -0700

    changes made

--- src/components/about/landing.tsx
+++ src/components/about/landing.tsx
@@ -8,76 +8,86 @@ import Image from "next/image";
 const Landing = () => {
   return (
     <div>
-      <div className="from-shpe-blue-200 to-shpe-blue-150 relative z-0 -mt-30 hidden h-[570px] bg-gradient-to-b lg:block">
+      {/* DESKTOP */}
+      <section className="from-shpe-blue-200 to-shpe-blue-150 -mt-30 relative hidden overflow-hidden bg-gradient-to-b lg:flex lg:min-h-[570px] lg:flex-col lg:items-center lg:justify-center">
         <Image
           src={gearImage}
           alt="gear"
-          className="absolute top-[-8px] left-[51px] w-[300px] scale-x-[-1]"
+          className="absolute left-10 top-2 w-[300px] scale-x-[-1]"
         />
         <Image
           src={gearImage}
           alt="gear"
-          className="absolute top-[197px] left-[1182px] w-[300px]"
+          className="absolute right-10 bottom-10 w-[300px]"
         />
         <Image
           src={wrenchImage}
           alt="wrench"
-          className="absolute top-[153px] left-[917px] w-[167px] rotate-265"
+          className="absolute right-105 top-32 w-[167px] rotate-[265deg]"
         />
-        <div className="font-shpe-beachwood text-shpe-white-100 scale-y-125 py-[275px] text-center text-[70px] leading-[70px] font-bold">
-          ABOUT US
+        <div className="z-10 flex flex-col items-center pt-50 text-center">
+          <h1 className="font-shpe-beachwood text-shpe-white-100 scale-y-125 text-[70px] leading-[70px] font-bold">
+            ABOUT US
+          </h1>
+          <p className="font-shpe-univers-condensed text-shpe-white-100 mt-8 max-w-2xl text-[30px] leading-[35px]">
+            Scroll down to learn more about SHPE and what we offer!
+          </p>
+          <Image
+            src={downArrow}
+            alt="down arrow"
+            className="mt-6 w-[90px]"
+          />
         </div>
-        <div className="font-shpe-univers-condensed text-shpe-white-100 absolute top-[378px] left-[408px] text-center text-[30px] leading-[35px]">
-          Scroll down to learn more about SHPE and what we offer!
-        </div>
-        <Image
-          src={downArrow}
-          alt="down arrow"
-          className="absolute top-[431px] left-[722px] w-[90px]"
-        />
-      </div>
+      </section>
 
-      <div className="from-shpe-blue-200 to-shpe-blue-150 relative z-0 -mt-30 h-[300px] bg-gradient-to-b lg:hidden">
+      {/* MOBILE */}
+      <section className="from-shpe-blue-200 to-shpe-blue-150 -mt-30 relative overflow-hidden bg-gradient-to-b px-4 py-10 lg:hidden">
         <Image
           src={gearImage}
           alt="gear"
-          className="absolute top-[140px] left-[-28px] w-[150px] rotate-8"
+          className="absolute bottom-12 left-[-30px] w-[150px] rotate-6"
         />
         <Image
           src={wrenchImage}
           alt="wrench"
-          className="absolute top-[50px] left-[625px] w-[193px] rotate-265"
+          className="absolute right-[-40px] top-12 w-[200px] rotate-[265deg]"
         />
-        <div className="font-shpe-beachwood text-shpe-white-100 scale-y-125 py-[125px] text-center text-[50px] leading-[70px] font-bold">
-          ABOUT US
+        <div className="relative z-10 flex flex-col pt-15 items-center text-center">
+          <h1 className="font-shpe-beachwood text-shpe-white-100 scale-y-125 text-[50px] font-bold">
+            ABOUT US
+          </h1>
+          <p className="font-shpe-univers-condensed text-shpe-white-100 mt-6 text-[22px] leading-[30px]">
+            Scroll down to learn more about SHPE and what we offer!
+          </p>
+          <Image
+            src={downArrow}
+            alt="down arrow"
+            className="mt-4 w-[30px]"
+          />
         </div>
-        <div className="font-shpe-univers-condensed text-shpe-white-100 absolute top-[200px] left-[125px] text-center text-[25px] leading-[35px]">
-          Scroll down to learn more about SHPE and what we offer!
-        </div>
-        <Image
-          src={downArrow}
-          alt="down arrow"
-          className="absolute top-[250px] left-[375px] w-[30px]"
-        />
-      </div>
+      </section>
 
-      <div className="relative min-h-screen">
+      {/* GROUP IMAGE SECTION */}
+      <section className="relative lg:flex lg:justify-center px-4 py-20">
         <Image
           src={gearImage}
           alt="gear"
-          className="rotate-5.41 absolute top-[78px] left-[69px] hidden w-[200px] opacity-70 lg:block"
+          className="absolute left-24 top-25 hidden w-[200px] rotate-6 opacity-70 lg:block"
         />
         <Image
           src={gearImage}
           alt="gear"
-          className="absolute top-[341px] left-[1195px] hidden w-[200px] scale-x-[-1] opacity-70 lg:block"
+          className="absolute right-24 bottom-22 hidden w-[200px] -rotate-10 scale-x-[-1] opacity-70 lg:block"
         />
-        <Image
-          src={groupPic}
-          alt="group picture"
-          className="absolute top-[30px] left-1/2 z-10 h-[527px] w-[1094px] -translate-x-1/2 object-cover lg:rounded-3xl"
-        />
-      </div>
+        <div className="relative z-10 w-full lg:max-w-[1094px] aspect-[1094/527]">
+          <Image
+            src={groupPic}
+            alt="group picture"
+            fill
+            className="object-cover lg:rounded-3xl"
+          />
+        </div>
+      </section>
     </div>
   );
 };
