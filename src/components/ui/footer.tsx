2ffe02ceb08e5537eb333a6db8fa3ca8fa737f85
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-06-25 18:13:21 -0700

    Creating a Data Entry for SHPE Links

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,8 +6,9 @@ import instagramIcon from "@/public/footer/instagram.webp";
 import linkedinIcon from "@/public/footer/linkedIn.webp";
 import newsIcon from "@/public/footer/news.webp";
 import shpeFooterLogo from "@/public/footer/shpeFooterLogo.svg";
-
+import SocialLinks from "@/data/socialData";
 import Image from "next/image";
+import Link from "next/link";
 
 const iconSize = "h-[50px] w-[50px]";
 
@@ -21,12 +22,24 @@ const Footer = () => {
       />
 
       <div className="flex w-full justify-around md:w-auto md:justify-end md:gap-[15px]">
-        <Image src={instagramIcon} alt="Instagram" className={iconSize} />
-        <Image src={newsIcon} alt="News" className={iconSize} />
-        <Image src={linkedinIcon} alt="LinkedIn" className={iconSize} />
-        <Image src={emailIcon} alt="Email" className={iconSize} />
-        <Image src={discordIcon} alt="Discord" className={iconSize} />
-        <Image src={facebookIcon} alt="Facebook" className={iconSize} />
+        <Link href={SocialLinks.instagram}>
+          <Image src={instagramIcon} alt="Instagram" className={iconSize} />
+        </Link>
+        <Link href={SocialLinks.newsletter}>
+          <Image src={newsIcon} alt="News" className={iconSize} />
+        </Link>
+        <Link href={SocialLinks.linkedin}>
+          <Image src={linkedinIcon} alt="LinkedIn" className={iconSize} />
+        </Link>
+        <Link href={SocialLinks.email}>
+          <Image src={emailIcon} alt="Email" className={iconSize} />
+        </Link>
+        <Link href={SocialLinks.discord}>
+          <Image src={discordIcon} alt="Discord" className={iconSize} />
+        </Link>
+        <Link href={SocialLinks.facebook}>
+          <Image src={facebookIcon} alt="Facebook" className={iconSize} />
+        </Link>
       </div>
     </div>
   );
---
+++ src/data/socialData.ts
@@ -0,0 +1,9 @@
+const SocialLinks = {
+  instagram: "https://www.instagram.com/shpeucr/",
+  email: "shpeucr@gmail.com",
+  discord: "https://discord.com/invite/AbUrTPBja6",
+  linkedin: "https://www.linkedin.com/company/shpe-at-ucr/",
+  facebook: "https://www.facebook.com/groups/shpeucr/",
+  newsletter: "https://shpe.org/about-shpe/news/shpe-nation/",
+};
+export default SocialLinks;
